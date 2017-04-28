8057e4706399886f757189fc7d38138ef7d905b9
Your Name <you@example.com>
2017-04-27 20:40:07 -0700

    changes in cal_regression

--- util/cal_regression.py
+++ util/cal_regression.py
@@ -1,33 +1,40 @@
 import numpy as np
 import models
 from utils import load_data_from_file
+from keras import optimizers
+from keras.models import load_model
 
 DIR = 'training_food101.txt'
 TARGET_SHAPE = (256, 256, 3)
+load = True
 
 
 if __name__ == '__main__':
-
     model = models.alexnet_like( (256, 256, 3), 1, None)
+    opt = optimizers.RMSprop(lr=0.0001, rho=0.9, epsilon=1e-08, decay=0.0) 
     model.compile(
-        optimizer='rmsprop', loss='mse')
-
+        optimizer=opt, loss='mse')
+    if load:
+        model = load_model('regressionfood101.h5')
+    print 'here 1'
     with open('validation_food101.txt') as f:
         valx, valy = next(load_data_from_file(f, 1000, target_shape=TARGET_SHAPE))
     
-    trainx = []
-    trainy = []
+    print 'here 2'
+    counter = 0
     with open(DIR) as f:
-        for x, y in load_data_from_file(f, 32, target_shape=TARGET_SHAPE):
-            trainx.append(x)
-            trainy.append(y)
-    trainx = np.vstack(trainx)
-    trainy = np.concatenate(trainy)
-    print 'HERE', trainx.shape, trainy.shape
-    
-
-    for i in range(10):
-    	model.fit(trainx, trainy, epochs=3, batch_size=32)
-        model.save('regressionfood101.h5')
+        for i in range(10):
+            losses = []
+            for x, y in load_data_from_file(f, 64, target_shape=TARGET_SHAPE):
+        	loss = model.train_on_batch(x, y)
+                losses.append(loss)
+                counter += 1
+                if counter % 20 == 0:
+                    print 'train', sum(losses) / float(len(losses))
+                    losses = []
+                    print 'validataion', np.mean((model.predict(valx) - valy) ** 2)
+            print 'saving'
+            model.save('regressionfood101.h5')
+            f.seek(0)
     
  
